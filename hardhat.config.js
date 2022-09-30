cd07225fda4eaa1d56ebe07a51f9698942eee634
timothyshen <timothyshen1@hotmail.com>
2022-09-30 23:26:06 +0100

    - Updated the deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({path: "./.env"});
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 // You need to export an object to set up your config
@@ -18,14 +19,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    hardhat: {
-        chainId: 1337
+    solidity: "0.8.4",
+    networks: {
+        hardhat: {
+            chainId: 1337
+        },
+        goerli: {
+            url: process.env.ALCHEMY_API_KEY_URL_GOERLI,
+            accounts: [process.env.PRIVATE_KEY]
+        }
     },
-    goerli: {
-        url: process.env.ALCHEMY_API_KEY_URL_GOERLI,
-        accounts: [process.env.PRIVATE_KEY]
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY
     }
-  }
 };
--- scripts/DeployInit.js
+++ scripts/DeployInit.js
@@ -1,8 +1,15 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+    // Deploying VME50 contract
+    const MyVME50 = await ethers.getContractFactory("VMe50");
+    const myVME50 = await MyVME50.deploy();
+    await myVME50.deployed();
+    console.log("VMe50 deployed to:", myVME50.address);
+
+    // Deploying USDChicken contract
     const MyUSDCHK = await ethers.getContractFactory("USDChicken");
-    const MyUSDCHKV1 = await MyUSDCHK.deploy();
+    const MyUSDCHKV1 = await MyUSDCHK.deploy(myVME50.address);
     await MyUSDCHKV1.deployed();
     console.log("MyUSDCHKV1 deployed to:", MyUSDCHKV1.address);
 
